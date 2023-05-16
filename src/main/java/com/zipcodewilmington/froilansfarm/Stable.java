620e013395260beca1061643b1ac878185a6223e
chrisv0303 <127235554+chrisv0303@users.noreply.github.com>
2023-05-16 19:31:33 -0400

    Update Stable.java

--- src/main/java/com/zipcodewilmington/froilansfarm/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Stable.java
@@ -4,39 +4,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Stable{
+public class Stable {
 
     private final List<Horse> horses = new ArrayList<>();
 
-    public Stable(){
-
-    }
-
-    public Stable(Horse horse){
+    public Stable() {}
+    public Stable(Horse horse) {
         this.horses.add(horse);
     }
 
-    public Stable(Horse[] horses){
+    public Stable(Horse[] horses) {
         this.horses.addAll(Arrays.asList(horses));
     }
 
-    public Stable(List<Horse> horses){
+    public Stable(List<Horse> horses) {
         this.horses.addAll(horses);
     }
 
-    public List<Horse> getHorses(){
+    public List<Horse> getHorses() {
         return this.horses;
     }
 
-    public void storeHorse(Horse horse){
+    public void storeHorse(Horse horse) {
         this.horses.add(horse);
     }
 
-    public void storeHorses(Horse[] horses){
+    public void storeHorses(Horse[] horses) {
         this.horses.addAll(Arrays.asList(horses));
     }
 
-    public void storeHorses(List<Horse> horses){
+    public void storeHorses(List<Horse> horses) {
         this.horses.addAll(horses);
     }
 }
