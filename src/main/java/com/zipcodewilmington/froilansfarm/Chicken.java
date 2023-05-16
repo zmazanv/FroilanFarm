5ee19737690fb1fb7c0fee46e3b153967aa62e6e
chrisv0303 <127235554+chrisv0303@users.noreply.github.com>
2023-05-16 19:19:50 -0400

    Update Chicken.java

--- src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -4,19 +4,19 @@ public class Chicken extends Animal implements Produce {
 
     private boolean hasBeenFertilized;
 
-    public Chicken(){
+    public Chicken() {
         this.hasBeenFertilized = false;
     }
 
-    public Chicken(boolean hasBeenFertilized){
+    public Chicken(boolean hasBeenFertilized) {
         this.hasBeenFertilized = hasBeenFertilized;
     }
 
-    public void fertilize(){
+    public void fertilize() {
         this.hasBeenFertilized = true;
     }
 
-    public boolean isFertilized(){
+    public boolean isFertilized() {
         return this.hasBeenFertilized;
     }
 }
