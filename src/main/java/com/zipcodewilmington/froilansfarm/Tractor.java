6782662ddba02a817d71ecfb38bfde050357540e
ev3lasco <127235457+ev3lasco@users.noreply.github.com>
2023-05-17 10:07:50 -0400

    Update Tractor.java

--- src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
@@ -1,11 +1,9 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Tractor extends  Vehicle implements FarmVehicle{
-    public void operate(Farm farm) {
+public class Tractor extends  Vehicle implements FarmVehicle {
+    
+    public void operate(Farm farm) {}
 
-    }
-
-    public void harvest (Farm farm){
-
-    }
+    public void harvest (Farm farm){}
+    
 }
