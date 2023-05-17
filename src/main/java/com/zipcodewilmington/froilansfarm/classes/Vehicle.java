bf3602bcbe123f110e4dea0545b1c033db06f350
ev3lasco <127235457+ev3lasco@users.noreply.github.com>
2023-05-17 15:50:13 -0400

    Update Vehicle.java

--- src/main/java/com/zipcodewilmington/froilansfarm/classes/Vehicle.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/classes/Vehicle.java
@@ -3,4 +3,20 @@ package com.zipcodewilmington.froilansfarm.classes;
 import com.zipcodewilmington.froilansfarm.interfaces.NoiseMaker;
 import com.zipcodewilmington.froilansfarm.interfaces.Rideable;
 
-public abstract class Vehicle implements NoiseMaker, Rideable {}
+public abstract class Vehicle implements NoiseMaker, Rideable {
+
+  @Override
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void beDismounted() {
+
+    }
+
+    @Override
+    public void beMounted() {
+
+    }
+}
