6b551b711cc03f1adb7c5a2a2e4acb1a4e162806
Edwin Velasco <evelasco1423@gmail.com>
2023-05-17 12:59:33 -0400

    Vehicle update

--- src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
@@ -5,5 +5,19 @@ public class CropDuster extends Aircraft implements FarmVehicle {
     public void operate(Farm farm) {}
 
     public void fertilize (CropRow cropRow) {}
-    
+
+    @Override
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void beDismounted() {
+
+    }
+
+    @Override
+    public void beMounted() {
+
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
@@ -4,6 +4,4 @@ public class Tractor extends  Vehicle implements FarmVehicle {
     
     public void operate(Farm farm) {}
 
-    public void harvest (Farm farm) {}
-    
 }
