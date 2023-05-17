0e1fbda3b7ef943865398a3e32e1a9eafbe5cc60
zmazanv <95514881+zmazanv@users.noreply.github.com>
2023-05-17 16:55:04 -0400

    Update Tractor.java
    Resolve merge conflict with `main`.

--- src/main/java/com/zipcodewilmington/froilansfarm/classes/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/classes/Tractor.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.froilansfarm.classes;
 
 import com.zipcodewilmington.froilansfarm.interfaces.FarmVehicle;
 
-public class Tractor extends  Vehicle implements FarmVehicle {
+public class Tractor extends Vehicle implements FarmVehicle {
     
     public void operate(Farm farm) {}
 
