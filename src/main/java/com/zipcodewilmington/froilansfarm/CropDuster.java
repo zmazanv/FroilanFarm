e365a7a21127014fcca6bf10ac8c4a657cdbb788
ev3lasco <127235457+ev3lasco@users.noreply.github.com>
2023-05-17 10:04:57 -0400

    Update CropDuster.java

--- src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class CropDuster extends Aircraft implements FarmVehicle {
-    public void operate(Farm farm) {
+    
+    public void operate(Farm farm) {}
 
-    }
-
-    public void fertilize (CropRow cropRow){
-
-    }
+    public void fertilize (CropRow cropRow) {}
 }
