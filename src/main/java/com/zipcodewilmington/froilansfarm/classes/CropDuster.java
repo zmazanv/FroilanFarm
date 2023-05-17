a2717f541acd8bbf4280d0d5dd0525cd3cdb023e
zmazanv <95514881+zmazanv@users.noreply.github.com>
2023-05-17 16:54:16 -0400

    Update CropDuster.java
    Resolve merge conflict with `main`.

--- src/main/java/com/zipcodewilmington/froilansfarm/classes/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/classes/CropDuster.java
@@ -7,19 +7,5 @@ public class CropDuster extends Aircraft implements FarmVehicle {
     public void operate(Farm farm) {}
 
     public void fertilize (CropRow cropRow) {}
-
-    @Override
-    public void makeNoise() {
-
-    }
-
-    @Override
-    public void beDismounted() {
-
-    }
-
-    @Override
-    public void beMounted() {
-
-    }
+    
 }
