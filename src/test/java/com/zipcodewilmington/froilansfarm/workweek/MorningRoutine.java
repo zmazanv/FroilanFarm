31546bfcc562ef16314b8fa27015a6f54ad35314
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-05-21 12:49:15 -0400

    Create MorningRoutine class for testing Routine

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/workweek/MorningRoutine.java
@@ -0,0 +1,14 @@
+package com.zipcodewilmington.froilansfarm.workweek;
+
+import org.junit.Test;
+
+import com.zipcodewilmington.froilansfarm.Routine;
+
+public class MorningRoutine extends Routine {
+
+    @Test
+    public void testMorningRoutine() {
+        doMorningRoutine();
+    }
+
+}
