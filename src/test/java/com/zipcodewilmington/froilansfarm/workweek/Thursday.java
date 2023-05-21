5eceec75ee77eace8f8393063101970de2fab480
Darus Slah <darusslah@gmail.com>
2023-05-21 13:47:10 -0400

    Make test case for thursday

--- src/test/java/com/zipcodewilmington/froilansfarm/workweek/Thursday.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/workweek/Thursday.java
@@ -1,14 +1,35 @@
 package com.zipcodewilmington.froilansfarm.workweek;
 
+import com.zipcodewilmington.froilansfarm.classes.animals.Horse;
+import com.zipcodewilmington.froilansfarm.classes.animals.Stable;
+import com.zipcodewilmington.froilansfarm.classes.crops.CornStalk;
+import com.zipcodewilmington.froilansfarm.classes.crops.Crop;
+import com.zipcodewilmington.froilansfarm.classes.crops.CropRow;
+import com.zipcodewilmington.froilansfarm.classes.crops.EarOfCorn;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.zipcodewilmington.froilansfarm.Routine;
+
+import java.util.List;
 
 public class Thursday extends Routine {
 
     @Test
     public void testThursday() {
         doMorningRoutine();
-    }
+        tractor.operate(farm);
 
-}
+        for (CropRow cropRow : field.getContainedCropRows()){
+            List<Crop> crops = cropRow.getContainedCrops();
+            for (Crop crop : crops){
+                Assert.assertTrue(crop.isHarvested());
+
+
+                    }
+                }
+            }
+        }
+
+
+
