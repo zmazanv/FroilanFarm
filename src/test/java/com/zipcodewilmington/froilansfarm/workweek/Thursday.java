7be22a898266131fe4f7080ae1aba6eaa719887f
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-05-21 14:48:28 -0400

    Adding missing comments to Thursday test class

--- src/test/java/com/zipcodewilmington/froilansfarm/workweek/Thursday.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/workweek/Thursday.java
@@ -20,9 +20,13 @@ public class Thursday extends Routine {
         // Ensuring that Froilanda was able to find and get on her Horse
         Assert.assertTrue(farm.getStables().get(0).getHeldHorses().get(1).isMounted());
         // Froilan and Froilanda return after some hours and carry on with their day
+        // Froilan returns his Horse
         froilan.dismount(farm.getStables().get(0).getHeldHorses().get(0));
+        // Ensuring that Froilan was able to get off his Horse
         Assert.assertFalse(farm.getStables().get(0).getHeldHorses().get(0).isMounted());
+        // Froilanda returns her Horse
         froilanda.dismount(farm.getStables().get(0).getHeldHorses().get(1));
+        // Ensuring that Froilanda was able to get off her Horse
         Assert.assertFalse(farm.getStables().get(0).getHeldHorses().get(1).isMounted());
     }
 
